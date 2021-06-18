22caba449d803a308bbe8c6d673d3d22c35bc902
kfpro <njrfarhandasilva10@gmail.com>
2021-06-19 00:07:46 +0530

    Finalize Code and Application

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import folium
 import ee
 import geemap
 import os
+import cv2
 
 Map = geemap.Map()
 
@@ -118,7 +119,12 @@ def predict_forest_fire(x,y,month,ffmc,dmc,dc,temp):
     print(prediction)
     return prediction
 
-# @st.cache
+def load_img(img_name):
+    img = cv2.imread(img_name) # Image.open(img_name) 
+    st.image(img,width=700)
+
+import os
+ROOT_DIR = os.getcwd()
 def main_prediction():
     # st.title("Forest Fire Prediction")
     # st.markdown("""<center><img src='https://upload.wikimedia.org/wikipedia/commons/d/d8/Deerfire_high_res_edit.jpg' width='500px'></center>""",unsafe_allow_html=True)
@@ -132,115 +138,25 @@ def main_prediction():
     st.markdown(html_temp,unsafe_allow_html=True)
     st.markdown("""<br>""",unsafe_allow_html=True)
     img_top = """<center><img src="https://i.imgur.com/yOS7IGv.png" width="700px"></center>"""
-    st.markdown(img_top,unsafe_allow_html=True)
+    # st.markdown(img_top,unsafe_allow_html=True)
     st.markdown("""<br>""",unsafe_allow_html=True)
+    trash = """
     with st.echo():
         folium_static(Map)
+    """
+    load_img(os.path.join(ROOT_DIR,"results_fire.png"))
         
-# @st.cache
-def corr_plot():
-    # st.write("You selected",len(location),"locations")
-    
-    # st.text("see plots")
-    # df = pd.read_csv("forestfires.csv")
-    # st.dataframe(df)
-    
-    corr = df.corr()
-
-    mask = np.triu(np.ones_like(corr, dtype=np.bool))
-
-    # Set up the matplotlib figure
-    f, ax = plt.subplots(figsize=(5, 5))
-
-    # Generate a custom diverging colormap
-    cmap = sns.diverging_palette(220, 10, as_cmap=True)
-
-    # Draw the heatmap with the mask and correct aspect ratio
-    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
-                square=True, linewidths=.5, cbar_kws={"shrink": .5})
-    plt.title('Correlation Plot',size=15)
-
-    st.pyplot(f)
-
-@st.cache(suppress_st_warning=True)
-def pair_plot():
-    # df = pd.read_csv("forestfires.csv")
-
-    # fig = plt.figure(figsize=(3,4))
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-
-    # fig = plt.figure()
-    # sns.set_style('whitegrid')
-    sns.pairplot(df)
-    plt.title("Pair Plot")
-    # plt.show()
-    st.pyplot()
 
 
-def V_corr_plot():
-    # df = pd.read_csv("forestfires.csv")
-    corr_new_train=df.corr()
-    fig = plt.figure(figsize=(3,4))
-    sns.heatmap(corr_new_train[['area']].sort_values(by=['area'],ascending=False).head(60),vmin=-1, cmap='seismic', annot=True)
-    plt.title("Vertical Correlation plot")
-    st.pyplot(fig)   
-
-def threeD_Plot():
-    # df = pd.read_csv("forestfires.csv")
-    dmc = list(df['DMC'])
-    temp = list(df['temp'])
-    area = list(df['area'])
-    train_f1 = pd.DataFrame(dmc,columns=['Dmc'])
-    train_f2 = pd.DataFrame(temp,columns=['Temp'])
-    train_target = pd.DataFrame(area,columns=['Target'])
-    train_data = pd.concat([train_f1,train_f2,train_target],axis = 1)
-    # train_data.head(5)
-    sns.set_style("whitegrid", {'axes.grid' : False})
-
-    fig = plt.figure(figsize=(3,3))
-
-    ax = Axes3D(fig) # Method 1
-    # ax = fig.add_subplot(111, projection='3d') # Method 2
-
-    x = np.random.uniform(1,20,size=20)
-    y = np.random.uniform(1,100,size=20)
-    z = np.random.uniform(1,100,size=20)
-
-
-    ax.scatter(train_data['Dmc'], train_data['Temp'], train_data['Target'], c=train_data['Dmc'],marker='o')
-    ax.set_xlabel('Dmc')
-    ax.set_ylabel('Temp')
-    ax.set_zlabel('Target')
-    ax.set_title("3D Scatter plot of DMC,temp and area")
-
-    # plt.show()
-    st.pyplot(fig)
-
-def get_pie_plot(feature):
-  df['area'] = df['area'].apply(lambda x : np.log(x+1))
-  df1 = df.groupby(feature).sum()  
-  df1 = df1[df1.area >0 ]
-
-  theme = plt.get_cmap('hsv')
-  cs = theme(np.arange(len(df1.area))/len(df1.area))
-  fig, ax = plt.subplots(figsize = (3, 3))
-  plt.pie(df1.area,labels = df1.index, colors = cs)
-  plt.title("Relative amount of area affected per "+feature, fontsize = 14)
-  # plt.show()
-  st.pyplot(fig)
-
-def load_img(img_name):
-    img = Image.open(img_name)
-    st.image(img,width=800)
 
 #sidebar Edit
 # st.title("hello")
-html_img = """<center><img src="https://i.ibb.co/VY5wCkN/47480912-png.png" width="130px"></center>"""
+html_img = """<center><img src="https://i.imgur.com/irqQcYg.png" width="130px"></center>"""
 st.sidebar.markdown(html_img,unsafe_allow_html=True)
 
 st.sidebar.markdown("""## Navigation Bar: <br> """,unsafe_allow_html=True)
 st.markdown("""<br><br>""",unsafe_allow_html=True)
-red = st.sidebar.radio(" ",["Prediction","About the Project","Collaborators of the Project"])
+red = st.sidebar.radio(" ",["Prediction","About the Project"]) # ,"Collaborators of the Project"
 # st.markdown("""<br></br> <br>""",unsafe_allow_html=True)
 st.sidebar.markdown("""<br><br><br><br><br> Thank you for visiting the site🤗""",unsafe_allow_html=True)
 st.sidebar.markdown(""" [  Our Github Repository](https://github.com/khanfarhan10/ForestFireDetect)""",unsafe_allow_html=True)
